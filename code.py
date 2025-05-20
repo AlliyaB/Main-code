06c069b57f0b0a650d17056ad316db7dda3d5fb4
AlliyaB <buchanana@stu.otc.school.nz>
2025-05-20 13:21:02 +1200

    Switches between frames 20/05

---
+++ code.py
@@ -0,0 +1,7 @@
+# Import modules for the code to run.
+"""Allows the tkinter libary to be imported."""
+import tkinter as tk
+"""Allows the current date to be imported."""
+from datetime import date
+
+print("hello")
